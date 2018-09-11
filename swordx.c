54778e38b5f55c99cf1374d515142a39f13b79bb
Michele <michele.celozzi@outlook.it>
2018-09-11 23:20:17 +0200

    Update File

--- swordx.c
+++ swordx.c
@@ -35,47 +35,24 @@ int main(void)
 		storeString(swordx,buf);
 	}
 
-
-	/* chiude il file */
-	fclose(fd);
-
-	//create the file swordnordered.out (it isn't ordered)		
-	int file_created;
-	int i;
-	file_created=creat("swordnordered.out", 0644);
-	if(file_created == 1) {printf("error");}
-	
-	//open the file sword.out 
-	
-	FILE *fp;
-	
-	fp = fopen("./swordnordered.out", "a");
-	
-	if (fp == NULL){
+	 
+	 if (fd == NULL){
 		exit(-1);
-		}
-		
-	//write on the file all word and occurrence
-	
-	node *po = swordx -> first; //returns first node
-	
-	while (i < countWord(buf))
-	{
-		fputs((po -> word), fp);
+	 }
+	 
+	 /* write on the file all word and occurrence*/
+	 node *po = last-> first;
+	  
+	 while (po!= NULL) {
+		fputs((po -> word), fd);
 		int number = (po -> occurrence);
-		fprintf(fp, " %d \n", number);
+		fprintf(fd, " %d \n", number);
 		po = (po -> next);
+	  };
+	  
+	  fflush(fd);
+	  fclose(fd);
 	}
-
-	{
-		fputs((po -> word), fp);
-		int number = (po -> occurrence);
-		fprintf(fp, " %d \n", number);
-		po = (po -> next);
-	} while (po -> next != NULL);
-		
-	fflush(fp);
-	fclose(fp);
 	
 	//write this on the shell
 	
