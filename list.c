6226f82386581e690f0bd9ad4b601c4e55385095
Michele <michele.celozzi@outlook.it>
2018-09-12 23:40:46 +0200

    Update List

--- list.c
+++ list.c
@@ -1,102 +1,65 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "list.h"
 
-int size = 0;
-
-list* createList(){
-	list * l = malloc(sizeof(list));
+List* createList()
+{
+	List* list = malloc(sizeof(List));
+	list -> first = NULL;
+	list -> last = NULL;
 	
-	l -> first = NULL;
-	l -> last = NULL;
-	
-	printf("lista creata! \n");
-	
-	return l;
+	return list;
 }
 
-
-void addWord(list* lst, char* str){
-	node* app = find(lst, str);
-	if(app == NULL){
-		node *n = createNode(str);
-		//if size = 0,non ha elementi ed il first è uguale al last
-		if (size == 0){
-			lst -> first  = n;
-			lst -> last = n;
-			}
-		else {
-			//lst -> last -> next = n;
-			lst -> last = n;	
+void addWord(List* l, char* c)
+{
+	node* app = find(l,c);
+	
+	if(app == NULL)
+	{
+		node* n = createNode(c);
+		
+		if((l -> first) == NULL)
+		{
+			l -> first = n;
+			l -> last = n;
 		}
+		else
+		{
+			l -> last = n;
+		}
+		
+		printf("\nFirst: %s\n",l -> first -> word);
+		printf("\nLast: %s\n",l -> last -> word);
 	}
-	else{
+	else
+	{
 		updateOccurrence(app);
 	}
-	size++;
-printf("\nFirst: %s\n",lst -> first -> word);
-printf("Last: %s\n",lst -> last -> word);
-	
-	
-	
-	//node *n = createNode(str);
-	//printf("\n la size è %i \n", size);
-	//if (size == 0){
-		//lst -> first = n;
-		//lst -> last = n;
-		//printf("\n First: %s\n",lst -> first -> word);
-		//printf("Last: %s\n",lst -> last -> word); 
-		//}
-	//else if (size == 1){
-		//printf("\n First: %s\n",lst -> first -> word);
-		//printf("Last: %s\n",lst -> last -> word);
-		//}
-	//size++;
-	
-	
-	
-	
-	
-	//if (size == 0){
-		//l -> first = n;
-		//l -> last = l ->first;
-		//}
-	//else if (size > 0){
-		//node *app = find(l, str);
-		//if (app == NULL){
-			//printf("\n First: %s\n",l -> first -> word);
-			//l -> last -> next = n;
-			//l -> last = n;
-			//}
-		//else {
-			//updateOccurrence(app);
-		//}
-	//} */
-	//size++;
-	//printf("\n First: %s\n",l -> first -> word);
-	//printf("Last: %s\n",l -> last -> word); 
-	}
-	
-node* find(list* l, char* str){
-	if (size == 0) return NULL;
-	node* n = l -> first;
-    while (n -> next != NULL){
-        if (strlen(str) == strlen(n -> word)){
-            int cmp = strcmp(str, n -> word);
-            
-            if (cmp == 0){
-                return n;
-            }
-        }
-        n = n -> next;
-    }
-    return NULL;
 }
 
-void updateOccurrence(node* n){
-	n -> occurrence +=1;
+node* find(List* l, char* str)
+{
+	node* n = l -> first;
+	
+	if(n == NULL)
+		return NULL;
+	
+	while(n -> next != NULL)
+	{
+		int cmp = strcmp(str,n -> word);
+		
+		if(cmp == 0)
+			return n;
+		
+		n = n -> next;
 	}
+	
+	return NULL;
+}
 
-
-
+void updateOccurrence(node* n)
+{
+	n -> occurrence += 1;
+}
