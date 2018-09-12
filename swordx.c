2d11bd42be3777311dc54d1f5c549e1709556a83
Michele <michele.celozzi@outlook.it>
2018-09-12 22:21:12 +0200

    add things

--- swordx.c
+++ swordx.c
@@ -5,24 +5,24 @@
 #include <getopt.h>
 
 #include "utils.h"
+#include "global.h"
 
 /* flags */
 
-static int recursive_flag;
-static int follow_flag;
-static int alpha_flag;
+int recursive_flag;
+int follow_flag;
+int alpha_flag;
+
 static int sort_flag;
 
-static char *fileToExclude;
-static int numMin;
-static char *fileToIgnore;
-static char *logFile;
+char *fileToExclude;
+int numMin;
+char *fileToIgnore;
+char *logFile;
+
 char *outputFile = "sword.out";
 
 void sort();
-
-
-//static char **files;
 
 /*
  * argc num parametri
@@ -90,7 +90,6 @@ int main (int argc, char *argv[]) {
 				abort ();
 			 }
 	}
-	
 /* Print any remaining command line arguments (not options). sono quindi i file di input */
   if (optind < argc)
     {
@@ -101,20 +100,18 @@ int main (int argc, char *argv[]) {
     while (optind < argc){
 		files[z] = argv[optind];
 		optind++;
-		printf ("%s ", files[z]);     
+		printf ("%s ", files[z]);   
 		putchar ('\n');
 		z++;
 		}
-	
 	for (int j= 0; j < z; j++){
-		updateList(sword, files[j]);
-		}	
+		checkName(sword, files[j]);
+		}
 	free(files);
-	
 	writeOnFile(sword, outputFile);
-	//sort();
-	
-    }
+	//sort(); */
+    } 
+  else("There aren't input files");
   exit (0);
 }
 
