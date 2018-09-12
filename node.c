8a78c5ed1d58dd7398d3214242ce60b647f90118
Michele <michele.celozzi@outlook.it>
2018-09-12 23:41:01 +0200

    Update Node

--- node.c
+++ node.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "node.h"
 
 //create node
 
-node* createNode(char* word1){
+node* createNode(char* w)
+{
 	node* n = malloc (sizeof(node));
 	
-	n -> word = word1;
+	n -> word = w;
 	n -> occurrence = 1;
 	n -> next = NULL;
 	
-	if(strlen(word1) == strlen(n -> word));
-	else
-		printf("Failed \n");
 	return n;
-	} 
+}
