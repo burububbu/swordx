1b573657b3c11baf568d405f6afb6651cb8015f5
Michele <michele.celozzi@outlook.it>
2018-09-12 22:21:43 +0200

    add things

--- utils.c
+++ utils.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include "utils.h"
+#include "global.h"
 
+int recursive_flag;
+int follow_flag;
+int alpha_flag;
+char *fileToExclude;
+int numMin;
+char *fileToIgnore;
+char *logFile;
+
+
+/* controlla che il nome sia un file o una directory, nel caso in cui fosse una directory */
+void checkName(list* lst, char* filename){
+	/* se NON è una directory fa l'update della lista passa a updateList il file, se no
+	 *  apre la directory, mette tutti i file nella directory in un'array di file
+	 * fa l'updateList su ognuno di loro */
+	 
+	 // 0 false 1 true
+	if(isDirectory(filename)== 0){
+		updateList(lst, filename);
+	}
+	else{
+		fileInDirUpdate(lst, filename, 0); 
+		}
+	}
 
 void updateList(list* lst, char* filename){
 	FILE *fd;
 	char buf[40];
-	/*open the file*/
 	fd = fopen(filename, "r");
 	
 	if( fd==NULL ){
@@ -14,19 +42,14 @@ void updateList(list* lst, char* filename){
 		exit(1);
 	}
 	else {
-	//printf("\n non ci sono problemi con l'apertura del file %s", filename);}
     /* assumes no word exceeds length of 40 */
     while (fscanf(fd, " %40s", buf) == 1) {
-		printf("ritorna nel while");
-        printf("\n %s", buf);
-        //IL PROBLEMA E' QUI
         addWord(lst, buf);
     }
-}
+} 
 }
 
 void writeOnFile(list* lst, char* outputFile){
-	/* open/create output file */	
 	FILE *fp;
 	fp = fopen(outputFile ,"a");
 	if (fp == NULL){
@@ -44,4 +67,79 @@ void writeOnFile(list* lst, char* outputFile){
 	fclose(fp);
 }
 
+/*sub is 0 if the dir is a subdir, 1 otherwise
+ * 1 -> not recursion*/
+int fileInDirUpdate (list* lst, char* filename, int sub){
+	DIR *dp;
+	struct dirent *ep;
+	dp = opendir (filename);
+	if (dp != NULL)
+    { 
+		while (ep = readdir (dp)){
+			/* EXCLUDE FILE */
+			if ((fileToExclude != NULL) && (strcmp(ep -> d_name, fileToExclude) == 0)){
+				 printf ("\n escludo il file %s dalla statistica\n", ep -> d_name);
+			}
+			else {
+				int sizePath =  strlen(filename) + strlen(ep -> d_name);
+				char name[sizePath];
+				strcpy(name, filename);
+				strcat(name, ep -> d_name );
+				if (isRegular(name) == 1){
+					printf("%s è regolare\n", name);
+					updateList(lst, name);
+					}
+				else {
+					 printf("/n%s non è regolare\n", name); 
+					 if (name[strlen(name)-1] != '.'){  /*ultimo char è .*/
+					 if (sub == 0){
+						 /*RECURSIVE*/
+						if(recursive_flag == 1){
+							if (!isLink(name))
+							{
+							char newname[sizePath + 1];
+							strcpy(newname,name);
+							strcat(newname, "/");
+							if (isDirectory(newname)) fileInDirUpdate(lst, newname, 1);
+							}
+						}
+						 /*FOLLOW*/
+						else if ((follow_flag == 1) && isLink(name)){
+							char newname[sizePath + 1];
+							strcpy(newname,name);
+							strcat(newname, "/");
+							fileInDirUpdate(lst, newname, 1);
+							} 
+						 } 
+					}
+				}
+		} 
+	}
+  (void) closedir (dp);
+  }
+  else {
+    perror ("Couldn't open the directory");
+   }
+  return 0;
+}
+
+
+int isDirectory(char *path) {
+   struct stat statbuf;
+   if (stat(path, &statbuf) != 0)
+       return 0;
+   return S_ISDIR(statbuf.st_mode);
+}
+
+int isRegular(char* path){
+	struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISREG(path_stat.st_mode);
+	}
+
+int isLink(char* path){
+	 struct stat path_stat;
+	 lstat(path, &path_stat);
+	 return S_ISLNK(path_stat.st_mode);
+	}
 
