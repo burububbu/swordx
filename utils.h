327b87577727a0003df39c52ddf6817e61cb1b97
Michele <michele.celozzi@outlook.it>
2018-09-12 22:22:03 +0200

    add things

--- utils.h
+++ utils.h
@@ -1,4 +1,10 @@
 #include "list.h"
 
+void checkName(list*, char*);
 void updateList(list*, char*);
 void writeOnFile(list*, char*);
+int isDirectory(char*);
+int isRegular(char*);
+int isLink(char*);
+int fileInDirUpdate(list*, char*, int); /*lista, file name, boolean is is a  sub or not*/
+char* createPath (char*, char*);
