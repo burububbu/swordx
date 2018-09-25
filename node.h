2203caeb2e90175194984264a83050905379c1bc
Michele <michele.celozzi@outlook.it>
2018-09-25 13:09:05 +0200

    Update Node Ignore

--- node.h
+++ node.h
@@ -6,6 +6,7 @@ typedef struct node{
 
 node *createNode();
 node* addWord(node*, char*);
+node* find(node*, char*);
 void updateOccurrence(node*);
 node* storeString(node*, char*, int, int);
 char* alphaControl(int , int);
