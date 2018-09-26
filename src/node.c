8d365b4b324fb1f075ff2e499ed704d1168d986a
Michele <michele.celozzi@outlook.it>
2018-09-27 01:42:59 +0200

    Update Swordx

--- src/node.c
+++ src/node.c
@@ -19,26 +19,20 @@ node* createNode(char* str)
 node* addWord(node* n, char *str)
 {	
 	if (n == NULL)
-	{
 		return createNode(str);
-	}
+	
 	while(n -> next != NULL)
 	{
 		if (strcmp(str, n -> word) == 0)
-		{
 			return updateOccurrence(n);
-		}
+		
 		n = n -> next;
 	}
 	/*contorlla ultimo nodo*/
 	if(strcmp(str, n -> word) == 0)
-	{
 		return updateOccurrence(n);
-		}
 	else
-	{
 		return n -> next = createNode(str);
-	}
 	
 	return NULL;
 }
@@ -53,52 +47,23 @@ node* updateOccurrence(node* n)
  * s[] : stringa letta
  * af : alpha flag
  * num : num min
- * wordstoignore : pointer to an array of pointer with parole da ignorare
+ * wordsToIgnore : pointer to an array of pointers to words to ignore
  * */
 node* storeString(node* first, char s[], int af, int num, char ** wordsToIgnore)
 {
-	if (wordsToIgnore != NULL) printf("\nIgnoro delle parole in un file\n");
+	if (wordsToIgnore != NULL) 
+		printf("\nIgnoro delle parole in un file\n");
+	
 	char* str= calloc(strlen(s),sizeof(char)); 
 	
 	int i = 0;
 	
 	while(s[i] != '\0')
-	{
-		/*if (isalpha(s[i]))
-		{
-			str[i] = tolower(s[i]);
-			}*/
-		//nel caso in cui può essere un numero ED è un numero
-		/*else if (af == 0)
-		{
-			if (isdigit(s[i]))
-			{
-				str[i] = s[i];
-			}
-			else
-			{
-				return NULL;
-				}
-		} 
-		
-		else
-			{
-				//str = NULL;
-				//break;
-				return NULL;
-			} 
-		else if(strlen(str) == 0)
-		{
-			str = NULL;
-			break;
-		} */
-		
-		if(af == 0)
+	{		
+		/*if(af == 0)
 		{
 			if(isalpha(s[i]) || isdigit(s[i]))
-			{
 				str[i] = tolower(s[i]);
-			}
 			else if(strlen(str) == 0)
 			{
 				str = NULL;
@@ -108,70 +73,83 @@ node* storeString(node* first, char s[], int af, int num, char ** wordsToIgnore)
 		else
 		{
 			if(isalpha(s[i]))
-			{
 				str[i] = tolower(s[i]);
-			}
 			else if(isdigit(s[i]))
-			{
 				return NULL;
-			}
 			else if(strlen(str) == 0)
 			{
 				str = NULL;
 				break;
 			}
+		}*/
+		if(isalpha(s[i]))
+			str[i] = tolower(s[i]);
+		else if(isdigit(s[i]))
+		{
+			if(af == 0)
+				str[i] = s[i];
+			else
+				return NULL;
+		}
+		else if(strlen(str) == 0)
+		{
+			str = NULL;
+			break;
 		}
 		
-			
 		i++;
-		}	
+	}
+	
 	if(str != NULL)
 	{
 		if(num == 0)
 		{
 			printf("*%s*\n",str);
-			if ((wordsToIgnore!= NULL) && isIgnored(str, wordsToIgnore))
-			{
-				return NULL;
-			}
-			else 
-			{
-			return addWord(first,str);
-			}
+			return handleIgnored(str, wordsToIgnore);
 		}
 		else
 		{
 			if(strlen(str) >= num)
 			{
 				printf("*%s*\n",str);
-				if ((wordsToIgnore != NULL) && isIgnored(str, wordsToIgnore))
-				{
-					return NULL;
-				}
-				else 
-				{
-				return addWord(first,str);
+				return handleIgnored(str, wordsToIgnore);
 			}
 		}
-	} 
 	}
+	
 	return NULL;
 }
 
-int isIgnored(char* str, char** wordsToIgnore){
-	int i = 0;
-	while (wordsToIgnore[i] != NULL){
-		if (strcmp(str,wordsToIgnore[i]) == 0) return 1;
-		i++;
-	 }
-	return 0;
-	}
+node* handleIgnored(char* s, char** wTI)
+{
+	if ((wTI != NULL) && isIgnored(str, wTI))
+		return NULL;
+	else
+		return addWord(first,str);
+}
 
-	
-void printIgnore(char** wordsToIgnore){
+int isIgnored(char* str, char** wordsToIgnore)
+{
 	int i = 0;
-	while (wordsToIgnore[i] != NULL){
+	while (wordsToIgnore[i] != NULL)
+	{
+		if (strcmp(str,wordsToIgnore[i]) == 0) 
+			return 1;
+		
+		i++;
+	}
+	 
+	return 0;
+}
+	
+void printIgnore(char** wordsToIgnore)
+{
+	int i = 0;
+	
+	while (wordsToIgnore[i] != NULL)
+	{
 		 printf("\n%s \n", wordsToIgnore[i]);
 		 i++;
-	 }
-	};
+	}
+	
+}
